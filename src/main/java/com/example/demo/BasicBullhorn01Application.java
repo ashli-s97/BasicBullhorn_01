6f58bf632aa2b3bf22b3fc2f3cc5ef987eec4a28
Ashlis-97 <ashli.spaulding66@gmail.com>
2017-11-27 16:49:17 -0500

    first commit

---
+++ src/main/java/com/example/demo/BasicBullhorn01Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicBullhorn01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicBullhorn01Application.class, args);
+	}
+}
---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,59 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    PostRepository postRepository;
+
+    @RequestMapping("/")
+    public String listPosts(Model model){
+        model.addAttribute("posts", postRepository.findAll());
+        return "list";
+    }
+
+    @GetMapping("/add")
+    public String postform (Model model){
+        model.addAttribute("post", new Post());
+        return "postform";
+    }
+
+    @PostMapping("/process")
+    public String processForm(@Valid Post post, BindingResult result)
+    {
+        if (result.hasErrors()){
+            return "postform";
+        }
+        postRepository.save(post);
+        return "redirect:/";
+    }
+
+    @RequestMapping("/detail/{id}")
+    public String showPost(@PathVariable("id") long id, Model model){
+        model.addAttribute("post", postRepository.findOne(id));
+        return "show";
+    }
+
+    @RequestMapping("/update/{id}")
+    public String updatePost(@PathVariable("id") long id, Model model){
+        model.addAttribute("post", postRepository.findOne(id));
+        return "postform";
+    }
+
+    @RequestMapping("/delete/{id}")
+    public String delCourse(@PathVariable("id") long id){
+        postRepository.delete(id);
+        return "redirect:/";
+    }
+}
